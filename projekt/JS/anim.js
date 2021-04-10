6989c105f5432695ca540f20c9e3d5b3306bd22b
ninib263 <70194102+ninib263@users.noreply.github.com>
2021-04-10 22:25:38 +0200

    Delete projekt directory

--- projekt/JS/anim.js
+++
@@ -1,7 +0,0 @@
-const many = document.querySelector('#mobil-meny')
-const menyLink = document.querySelector('.toppnav_meny')
-
-many.addEventListener('click', function(){
-    many.classList.toggle('is-active');
-    menyLink.classList.toggle('active');
-});
